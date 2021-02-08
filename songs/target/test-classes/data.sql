eeda6a3da91d26c47f24015cf9610553037b02f9
enricoDec <enricodechadarevian@gmail.com>
2021-02-08 19:18:36 +0100

    remove target (ups)

--- songs/target/test-classes/data.sql
+++
@@ -1,4 +0,0 @@
-delete from users;
-
-insert into users(userid, password, firstname, lastname) values ('mmuster', 'pass1234', 'Maxime', 'Muster');
-insert into users(userid, password, firstname, lastname) values ('admin', 'admin', 'Max', 'Mustermann');
