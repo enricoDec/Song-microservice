01969f186bda56d2f4a60ea58e1c567f37fe97b7
enricoDec <enricodechadarevian@gmail.com>
2021-02-10 15:31:26 +0100

    add javadoc

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"htwb.ai.controller"},{"l":"htwb.ai.controller.controller"},{"l":"htwb.ai.controller.model"},{"l":"htwb.ai.controller.repo"},{"l":"htwb.ai.controller.utils"},{"l":"integration"}]
