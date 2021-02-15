fbe196d40bd57250dc8cb354293f24c8bd1e245f
enricoDec <enricodechadarevian@gmail.com>
2021-02-15 18:05:26 +0100

    implement delete ticket

--- concerts/src/main/java/htwb/ai/controller/controller/TicketsController.java
+++ concerts/src/main/java/htwb/ai/controller/controller/TicketsController.java
@@ -34,6 +34,19 @@ public class TicketsController {
         this.concertsRepository = concertsRepository;
     }
 
+    /**
+     * Get ticket of authorized user
+     * Example:
+     * Get ../songWS/rest/tickets
+     * <p>
+     * Get a new Ticket
+     * Example:
+     * Get ../songWS/rest/tickets?buyConcert={id}
+     *
+     * @param jwt       Jwt Token
+     * @param concertId id of concert to get ticket
+     * @return owner tickets or 200 if ticket added
+     */
     @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<List<Ticket>> getOwnedTickets(@RequestHeader(value = "Authorization", required = false) String jwt,
                                                         @RequestParam(value = "buyConcert", required = false) Long concertId) {
@@ -61,13 +74,50 @@ public class TicketsController {
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
             //Check if tickets available
-            //TODO: Should avoid user getting more than one ticket?
             if (concert.getTickets().size() < concert.getMaxTickets()) {
                 Ticket ticket = new Ticket(claims.getId(), concert, new TicketTransaction(false));
                 ticketsRepository.save(ticket);
                 return new ResponseEntity<>(HttpStatus.OK);
             } else
                 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    /**
+     * Delete a specific Ticket
+     * Only authorized owner of playlist can delete his own playlists
+     * Example:
+     * Delete ../songsWS/rest/ticket/{id}
+     *
+     * @param jwt      Jwt token
+     * @param ticketId id of ticket to be deleted
+     * @return HTTP NO CONTENT if playlist has been deleted, UNAUTHORIZED if not authorized,
+     * FORBIDDEN if trying to delete playlist that belongs to another user or BAD REQUEST
+     */
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<String> deleteTicket(@RequestHeader(value = "Authorization", required = false) String jwt,
+                                               @PathVariable(value = "id") Long ticketId) {
+        if (ticketId == 0)
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        Claims claims;
+        try {
+            claims = JwtDecode.decodeJWT(jwt);
+        } catch (ExpiredJwtException | UnsupportedJwtException |
+                MalformedJwtException | SignatureException | IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        //If authenticated user request to delete his ticket
+        Ticket requestedTicket;
+        try {
+            requestedTicket = ticketsRepository.findById(ticketId).get();
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        if (claims.getId().equals(requestedTicket.getOwner())) {
+            ticketsRepository.deleteById(ticketId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
         }
     }
 }
